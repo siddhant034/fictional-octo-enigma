a8d6dcaa7033be9a597b517eeec44965b7faeadc
siddhant034 <shekhar.siddhant@gmail.com>
2018-02-13 22:44:36 +0530

    added welcome screen

---
+++ src/app/components/market/market.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'market',
+  templateUrl: './market.component.html',
+  styleUrls: ['./market.component.css']
+})
+export class MarketComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
